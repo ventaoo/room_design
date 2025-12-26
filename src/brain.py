a97290fa9b6a0bea196c02e7104c96fc4de9845a
ventaoo <ventaoczu@gmail.com>
2025-12-26 12:39:15 +0300

    To qwen3

--- src/brain.py
+++ src/brain.py
@@ -61,17 +61,11 @@ class DesignBrain:
         
         text = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True, enable_thinking=True) # Qwen3 0.6b thinking mode
         model_inputs = self.tokenizer([text], return_tensors="pt").to(self.device)
-        
+
         generated_ids = self.model.generate(
-            model_inputs.input_ids,
-            max_new_tokens=256,
-            temperature=0.1,
-            do_sample=False
+            **model_inputs,
+            max_new_tokens=32768
         )
-        
-        generated_ids = [
-            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-        ]
         output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
 
         # parsing thinking content
